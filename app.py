7c65b04b5a272a77f8eeb53892fa2a51e852ef11
PPravalika17 <22251a1221@gmail.com>
2025-07-09 10:43:42 +0530

    my third commit

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello():
-    return("Hello,welcome to Github")
+    return("Hello,Welcome to Github")
 if __name__=="__main__":
     app.run(debug=True)
